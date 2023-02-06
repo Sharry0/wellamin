a6091feb76c696c81a05db00feb988ad2380144c
Sharone Nixon <shanix@live.de>
2023-02-06 22:41:39 +0100

    fixed about &apos;

--- pages/about.jsx
+++ pages/about.jsx
@@ -6,18 +6,18 @@ export default function About() {
       <p>
         Welcome to [company name], a healthcare company on a mission to improve the overall health and well-being of humanity.
         We believe that a healthy life begins with a balanced and complete diet, and sometimes, despite our best efforts,
-        it can be difficult to get all the essential vitamins and minerals our bodies need. That's why we're proud to introduce
+        it can be difficult to get all the essential vitamins and minerals our bodies need. That&apos;s why we&apos;re proud to introduce
         our multivitamin supplement, designed to fill any nutritional gaps and provide a foundation for a healthy life.
 
         Our team of experts has carefully researched and formulated a blend of essential vitamins and minerals that work together
-        to support your body's overall health and wellness. Our formula is non-GMO, gluten-free, and contains no artificial colors,
+        to support your body&apos;s overall health and wellness. Our formula is non-GMO, gluten-free, and contains no artificial colors,
         flavors, or preservatives.
 
-        We understand the importance of trust in the supplements you take, and that's why we use only the highest quality ingredients,
+        We understand the importance of trust in the supplements you take, and that&apos;s why we use only the highest quality ingredients,
         manufactured in a cGMP-certified facility in the USA.
 
-        At [company name], we believe that a healthy world starts with healthy individuals, and we're committed to empowering people
-        everywhere to live their best lives. Whether you're looking to support your immune system, energy levels, or overall health,
+        At [company name], we believe that a healthy world starts with healthy individuals, and we&apos;re committed to empowering people
+        everywhere to live their best lives. Whether you&apos;re looking to support your immune system, energy levels, or overall health,
         our multivitamin is the perfect addition to your daily routine.
 
         Join us on our journey to a healthier world and experience the difference for yourself. Try [company name] multivitamin today!
