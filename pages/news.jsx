584e73869a9c2f5889fc7ade2cdf63aaead2ef3d
Sharone Nixon <shanix@live.de>
2023-03-01 13:01:45 +0100

    added images to article

--- pages/news.jsx
+++ pages/news.jsx
@@ -9,70 +9,70 @@ export default function News() {
       newsOutlet: "Horizon Times",
       newsOutletImg: "/news/newsOutlets/outlet_2.png",
       headline: "New vitamin pill from Wellamin could revolutionize daily nutrition",
-      img: "",
+      img: "/news/article/article_1.jpg",
       date: "November 25, 2022",
     },
     {
       newsOutlet: "Global Daily Times ",
       newsOutletImg: "/news/newsOutlets/outlet_4.png",
       headline: "Wellamin's vitamin pill shows promising results in clinical trials",
-      img: "",
+      img: "/news/article/article_2.jpg",
       date: "November 08, 2022",
     },
     {
       newsOutlet: "Horizon Times",
       newsOutletImg: "/news/newsOutlets/outlet_2.png",
       headline: "The new vitamin pill from Wellamin receives FDA approval",
-      img: "",
+      img: "/news/article/article_3.jpg",
       date: "October 18, 2022",
     },
     {
       newsOutlet: "Brighter Press",
       newsOutletImg: "/news/newsOutlets/outlet_3.png",
       headline: "Wellamin's vitamin pill offers affordable and convenient daily nutrition solution",
-      img: "",
+      img: "/news/article/article_4.jpg",
       date: "September 30, 2022",
     },
     {
       newsOutlet: "Sunrise Journal",
       newsOutletImg: "/news/newsOutlets/outlet_1.png",
       headline: "Experts praise Wellamin's vitamin pill as a game-changer in the health supplement industry",
-      img: "",
+      img: "/news/article/article_5.jpg",
       date: "September 23, 2022",
     },
     {
       newsOutlet: "Sunrise Journal",
       newsOutletImg: "/news/newsOutlets/outlet_1.png",
       headline: "Wellamin's vitamin pill contains innovative blend of nutrients for optimal health",
-      img: "",
+      img: "/news/article/article_6.jpg",
       date: "September 12, 2022",
     },
     {
       newsOutlet: "Brighter Press",
       newsOutletImg: "/news/newsOutlets/outlet_3.png",
       headline: "The new vitamin pill from Wellamin receives rave reviews from customers",
-      img: "",
+      img: "/news/article/article_7.jpg",
       date: "August 15, 2022",
     },
     {
       newsOutlet: "Global Daily Times ",
       newsOutletImg: "/news/newsOutlets/outlet_4.png",
       headline: "Wellamin's vitamin pill recognized as a top-performing health supplement in the market",
-      img: "",
+      img: "/news/article/article_8.jpg",
       date: "August 08, 2022",
     },
     {
       newsOutlet: "Brighter Press",
       newsOutletImg: "/news/newsOutlets/outlet_3.png",
       headline: "New study confirms efficacy of Wellamin's vitamin pill in promoting overall wellness",
-      img: "",
+      img: "/news/article/article_9.jpg",
       date: "July 23, 2022",
     },
     {
       newsOutlet: "Global Daily Times ",
       newsOutletImg: "/news/newsOutlets/outlet_4.png",
       headline: "Wellamin's vitamin pill offers vegan and gluten-free option for health-conscious consumers",
-      img: "",
+      img: "/news/article/article_10.jpg",
       date: "July 17, 2022",
     },
   ]
@@ -88,12 +88,12 @@ export default function News() {
             news.map((article, i) => (
               <div className={css.articleContainer} key={i} >
                 <div className={css.articleHead}>
-                  <Image src={article.newsOutletImg} width={40} height={40} />
+                  <Image src={article.newsOutletImg} width={40} height={40} alt={`Logo of ${article.newsOutlet}`} />
                   {article.newsOutlet}
                 </div>
                 <div className={css.articleBody}>
                   <div className={css.articleImg} >
-                    <Image src={"/news/news_bg2.jpg"} fill style={{ objectFit: "cover" }} />
+                    <Image src={article.img} fill sizes="100%" style={{ objectFit: "cover" }} alt={`${article.newsOutlet} article image`} priority={i < 4 ? true : false} />
                   </div>
                   <h3>{article.headline}</h3>
                 </div>
