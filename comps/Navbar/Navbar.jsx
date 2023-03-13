e93c4ae717670a66d5f8cbb8e68cf8e52f2d786f
Sharone Nixon <shanix@live.de>
2023-03-13 09:29:54 +0100

    added reponsive navbar

--- comps/Navbar/Navbar.jsx
+++ comps/Navbar/Navbar.jsx
@@ -2,10 +2,12 @@
 import css from "./Navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Navbar() {
   const router = useRouter();
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
   const activeNavlLink = {
     fontWeight: "bold",
     textDecoration: "underline"
@@ -24,6 +26,19 @@ export default function Navbar() {
           <Link href="/news" style={router.pathname == "/news" ? activeNavlLink : {}} >News</Link>
           <Link href="/about" style={router.pathname == "/about" ? activeNavlLink : {}} >About</Link>
           <Link href="/contact" style={router.pathname == "/contact" ? activeNavlLink : {}} >Contact</Link>
+        </div>
+        <div className={css.hamburgerContainer} onClick={() => setHamburgerOpen(!hamburgerOpen)} >
+          <button className={css.hamburger}  >
+            <Image src={"/hamburger.svg"} width={25} height={25} />
+          </button>
+          {hamburgerOpen &&
+            <div className={css.hamburgerCollapse}  >
+              <Link href="/science" style={router.pathname == "/science" ? activeNavlLink : {}} >Science</Link>
+              <Link href="/news" style={router.pathname == "/news" ? activeNavlLink : {}} >News</Link>
+              <Link href="/about" style={router.pathname == "/about" ? activeNavlLink : {}} >About</Link>
+              <Link href="/contact" style={router.pathname == "/contact" ? activeNavlLink : {}} >Contact</Link>
+            </div>
+          }
         </div>
       </nav>
     </div>
